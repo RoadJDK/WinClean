4f4215bc385a61e7862a9c284f43cbe5010c299a
Lukas Freckmann <lukas.freckmann@gmx.ch>
2021-06-16 18:28:59 +0200

    Auto Commit

--- publish.bat
+++ publish.bat
@@ -1,6 +1,5 @@
 dotnet publish -r win-x64 -p:PublishSingleFile=true --self-contained true
-rm WinClean.rar
-"%ProgramFiles%\WinRAR\WinRAR.exe" a WinClean.rar .\bin\Debug\netcoreapp3.1\win-x64\publish\WinClean.exe .\bin\Debug\netcoreapp3.1\win-x64\publish\WinClean.pdb
+"%ProgramFiles%\WinRAR\WinRAR.exe" a WinClean.rar .\bin\Debug\netcoreapp3.1\win-x64\publish\WinClean.exe -AP / .\bin\Debug\netcoreapp3.1\win-x64\publish\WinClean.pdb -AP /
 git add .
 git commit -m "Auto Commit"
 git pull
