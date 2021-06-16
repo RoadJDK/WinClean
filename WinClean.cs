842e4f93799cde37a79feac8519b5047761e8d14
Lukas Freckmann <lukas.freckmann@gmx.ch>
2021-06-16 16:05:43 +0200

    Initial Commit

---
+++ WinClean.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace WinClean {
+    class WinClean {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
