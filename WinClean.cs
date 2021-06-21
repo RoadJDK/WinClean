0f0be778e9869e2df08a82e3e018de0693657426
Lukas Freckmann <lukas.freckmann@gmx.ch>
2021-06-21 22:43:34 +0200

    Added parts

--- WinClean.cs
+++ WinClean.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using static WinClean.ConsoleHelper;
 
 namespace WinClean {
     /// <summary>
     /// The main method for the WinClean program.
     /// 
-    /// For Version: v0.0.1
+    /// For Version: v0.0.0
     /// Author: Akjo03
     /// </summary>
     public class WinClean {
@@ -12,8 +14,37 @@ namespace WinClean {
         public static string Version = "v0.0.0";
 
         public static void Main(string[] args) {
-            ConsoleHelper.ConsoleWrite("Hello World!");
-            ConsoleHelper.EnterToContinue();
+            // === Part 0 - Checking for Windows 10 ===
+
+
+            // === Part 1 - Activating Windows ===
+
+
+            // === Part 2 - Updating Windows ===
+
+
+            // === Part 3 - Cleaning Windows from Bloatware ===
+
+
+            // === Part 4 - Setting System Settings ===
+
+
+            // === Part 5 - Setting Security Settings ===
+
+
+            // === Part 6 - Installing Basic Software ===
+
+
+            // === Part 7 - Personalizing Windows ===
+
+
+            // === Part 8 - Installing Additional Software ===
+
+
+            // === Part 9 - Checking System Integrity ===
+
+
+            // === Part 10 - Finishing WinClean ===
         }
     }
 }
