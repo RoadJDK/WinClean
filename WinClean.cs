00051192b51047dac95369028eb901091f4ac8f8
Lukas Freckmann <lukas.freckmann@gmx.ch>
2021-06-21 21:54:16 +0200

    Changed version number to v0.0.1-alpha1

--- WinClean.cs
+++ WinClean.cs
@@ -9,7 +9,7 @@ namespace WinClean {
     /// </summary>
     public class WinClean {
 
-        public static string Version = "v0.0.0";
+        public static string Version = "v0.0.1-alpha1";
 
         public static void Main(string[] args) {
             ConsoleHelper.ConsoleWrite("Hello World!");
