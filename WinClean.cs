7a7297cf8bdbf888fc4a90baf9bc0e57d59f0300
Lukas Freckmann <lukas.freckmann@gmx.ch>
2021-06-22 16:52:22 +0200

    Changed version number again

--- WinClean.cs
+++ WinClean.cs
@@ -16,7 +16,7 @@ namespace WinClean {
     /// </summary>
     public class WinClean {
 
-        public static string Version = "v0.1.0-iss11-t1";
+        public static string Version = "v0.1.0-iss11-t2";
 
         public static void Main(string[] args) {
             ConsoleClear();
